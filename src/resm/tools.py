f820b209451303563e232a0ff48cc057a447035d
Konstantin Kulikov <rasmadeus@gmail.com>
2015-04-26 13:46:35 +0300

    Del: tools

--- src/resm/tools.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def get_options():
-    from optparse import OptionParser
-    parser = OptionParser()
-    parser.add_option("-n", "--number", type="int", default=3, help="Number of resources.")
-    parser.add_option("-p", "--port", type="int", default=5000, help="Web service port.")
-    return parser.parse_args()[0].__dict__
